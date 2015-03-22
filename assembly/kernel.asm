ac279db8bba38dcab3a6684313aaf0343273f029
Harry Maclean <harryjmaclean@googlemail.com>
2015-03-22 15:58:56 +0000

    enter keyboard input loop on boot

--- assembly/kernel.asm
+++ assembly/kernel.asm
@@ -2,7 +2,15 @@
 kernel_start:
 	mov bx, eric
 	call print_screen
+	jmp get_key
 	jmp hang
 
 ; String to print to screen.
 eric db "hello eric", 0
+
+get_key:
+	mov ah, 0
+	int 0x16
+	mov ah, 0x0e
+	int 0x10
+	jmp get_key
