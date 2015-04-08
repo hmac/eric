bb4b96b2cff7ce317da8c76d39f952dfc74bff04
Harry Maclean <harryjmaclean@googlemail.com>
2015-04-08 15:38:08 +0100

    Extract printing routines into separate file.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,19 +1,13 @@
 #include <stdint.h>
 #include <stdbool.h>
-
+#include "print.c"
 #include "gdt.c"
 
 void clear_screen();
 void* malloc(unsigned long size);
-void printchar(char c);
-void print(char str[]);
 
 #define VGA_START 0xb8000
-#define TERM_WIDTH 80
 #define HEAP_START 0x100000
-int term_column = 0;
-int term_row = 0;
-char term_colour;
 
 int main() {
   term_colour = *((char*) VGA_START+1);
@@ -30,29 +24,6 @@ int main() {
 
   return 0;
 }
-
-
-void print(char *str) {
-  for (int i = 0; str[i] != '\0'; i++) {
-    printchar(str[i]);
-  }
-}
-
-
-void printchar(char c) {
-  uint16_t  entry = c | term_colour << 8;
-  uint16_t* vga = (uint16_t*) VGA_START;
-  uint16_t index = (TERM_WIDTH*term_row) + term_column;
-  vga[index] = entry;
-  if (term_column == TERM_WIDTH) {
-    term_column = 0;
-    term_row++;
-  }
-  else {
-    term_column++;
-  }
-}
-
 
 void clear_screen() {
   char* start = (char*) VGA_START;
